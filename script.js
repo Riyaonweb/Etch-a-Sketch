f6163791a61ec64a797469887abfc2b031fa92bf
Riyaonweb <riyahyf@gmail.com>
2024-02-07 22:54:09 +0530

    Add working files

--- script.js
+++ script.js
@@ -1,21 +1,73 @@
 const container = document.getElementById("container");
-
+const value = document.getElementById("value");
+const black = document.getElementById("black");
+const pixel = document.getElementById("pixel");
+const colors = document.querySelectorAll(".color");
+function color(){
+    const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16).padStart(6,'0').toUpperCase();
+    console.log(randomColor);
+    return randomColor;
+}
 
 //16*16 grid
-function createDiv(){
-    for(let i=0; i<256; i++){
-        const div = document.createElement("div");
-        div.style.border = "1px solid black";
-        div.classList.add("active");
-        container.appendChild(div);
-        console.log(div)
-    }
+function getValue(){
+    value.addEventListener("click",() => {
+        var input = prompt("Enter number");
+        value.innerText = input+ "*" +input;
+        console.log("input", input);
+        defaultGrid(input);
+        return input;
+    })
 }
-createDiv();
 
-container.addEventListener("mouseover",(e) => {
-    if(e.target.classList.contains('active')){
-        e.target.style.backgroundColor = "black";
-    }
-});
+getValue();
+
+function defaultGrid(inputValue){
+console.log("inputValue", inputValue);
+for(let i=0; i<(inputValue*inputValue); i++){
+    const div = document.createElement('div');
+    div.style.border = "1px solid black";
+    div.classList.add("active");
+    let w = `calc(100% / ${inputValue})`;
+    let h = `calc(100% / ${inputValue})`;
+    div.style.width = w;
+    div.style.height = h;
+    container.appendChild(div);
+    console.log(div);
+}
+}
+
+function chooseColor(){
+    colors.forEach((color) => {
+        color.addEventListener("click",(e) => {
+            if(e.target.innerHTML === "Pixel"){
+                pixels();
+            }
+        else if(e.target.innerHTML === "Black"){
+            blacks();
+        }
+    })
+})
+}
+chooseColor();
+
+function blacks(){
+    container.addEventListener("mouseover",(e) => {
+        if(e.target.classList.contains('active')){
+            e.target.style.backgroundColor = "black";
+        }
+    }); 
+}
+function pixels(){
+    container.addEventListener("mouseover",(e) => {
+        if(e.target.classList.contains('active')){
+            e.target.style.backgroundColor = color();
+        }
+    });
+    
+}
+
+
+
+
 
